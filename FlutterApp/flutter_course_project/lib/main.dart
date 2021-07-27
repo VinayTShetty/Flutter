03863fe01284f827a45327dbbd0b82471d004760
Vinay T S <vinaytshetty@gmail.com>
2021-07-27 12:43:59 +0530

    Demo code showing the parenthesis of proper code refraction.

--- FlutterApp/flutter_course_project/lib/main.dart
+++ FlutterApp/flutter_course_project/lib/main.dart
@@ -3,15 +3,34 @@ import 'package:flutter/material.dart';
 /**
  * Application removed blck background containing only White space.
  */
-void main() => runApp(MyApp());
+void main() => runApp(DemoApp());
 
 class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar:AppBar(title: Text('My Frist App')),
-        body:Text('Hello World o')
-      ),
+          appBar: AppBar(title: Text('My Frist App')),
+          body: Text('Hello World o')),
     );
   }
 }
+
+class DemoApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+
+      home: Scaffold(
+
+      appBar: AppBar(title: Text('Demo another Widget'))      
+      ,
+      body: Text('Demo Widget example')
+
+    )
+    
+    );
+  }
+}
+
+
