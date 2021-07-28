14ddaa5c6087a114238c23e1e2ba14386ececce0
Vinay T S <vinaytshetty@gmail.com>
2021-07-28 07:03:30 +0530

    using properties in the Container Widget.

--- FlutterApp/flutter_course_project/lib/question.dart
+++ FlutterApp/flutter_course_project/lib/question.dart
@@ -8,10 +8,16 @@ class Question extends StatelessWidget {
   Question(this.questionText);
   @override
   Widget build(BuildContext context) {
-    return Text(questionText,
-        style: TextStyle(fontSize: 18), textAlign: TextAlign.center);
+    return Container(
+      width: double.infinity,
+      margin: EdgeInsets.all(10),
+      child:
+      Text(questionText,style: TextStyle(fontSize: 18), textAlign: TextAlign.center)) ;
   }
 }
 /**
  * Using this propert will not  align the text in the center.
+ * textAlign: TextAlign.center -->It will wrap the text with the minimum text length required.
+ * 
+ * By using another property i.e Container will make the text ailgn in the center.
  */
