0c222ab1ce48a2aa8e2eaafbbd733fe9e0cdbd0b
VinayTShetty <vinaytshetty@gmail.com>
2024-01-22 18:36:16 +0530

    Consumer Implementation in Provider. 1)With the help of Consumer,the entire widget build can be avoided. 2)Only the required widget will be build.

--- lib/screen/count_screen.dart
+++ lib/screen/count_screen.dart
@@ -13,15 +13,20 @@ class _State extends State<CounterScreen> {
   @override
   Widget build(BuildContext context) {
     print("Build Widget");
-    final countprovider = Provider.of<CountProvider>(context);
+    final countprovider = Provider.of<CountProvider>(context, listen: false);
     return Scaffold(
       appBar: AppBar(
         title: Text('Provider'),
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
       ),
       body: Center(
-        child: Text(countprovider.countValue.toString()),
-      ),
+          child: Consumer<CountProvider>(builder: (context, value, child) {
+        print("only this widget builds");
+        return Text(
+          value.countValue.toString(),
+          style: TextStyle(fontSize: 50),
+        );
+      })),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           countprovider.setCounter();
